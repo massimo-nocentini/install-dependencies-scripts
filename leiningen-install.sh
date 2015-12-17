48fd7dd551ec23b3278cbda7b905525918eaa7ec
Massimo Nocentini <massimo.nocentini@gmail.com>
2015-12-17 08:57:22 +0100

    Add a little more explanation for `leiningen`.

--- leiningen-install.sh
+++ leiningen-install.sh
@@ -2,7 +2,9 @@
 # in order to install `leiningen`, the tooling for clojure project development,
 # it is sufficient to get `lein` script at:
 # https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-# all clojure stuff then pass through it.
+# all clojure stuff then pass through it. Then make it executable (on the github page
+# there's a complete step by step description however), then add the directory
+# within our Developer/snapshot tree to the PATH system variable.
 
 # moreover its dependencies, namely jdk, are already satisfied at this point of our
 # main script.
