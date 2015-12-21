432503b7468be9ab060eaebfa83ae0f0335f38de
Massimo Nocentini <massimo.nocentini@gmail.com>
2015-12-21 18:19:01 +0100

    Refine script for `pure-lang`, changing the `configure` command to work with `gcc` and `llvm-3.5`.

--- pure-lang-install-dependencies.sh
+++ pure-lang-install-dependencies.sh
@@ -2,6 +2,15 @@
 # the following dependencies are well explained in the provided tarball.
 sudo apt-get install llvm-dev clang libclang-dev libgmp-dev libmpfr-dev libpcre3-dev libpcre++-dev libreadline-dev autoconf automake flex bison mercurial libltdl-dev 
 
+# the following are necessary in order to build Pure 0.64, 
+# the upmost version 3.6 makes a change in a header file 
+# that break Pure compilation.
+sudo apt-get install llvm-3.5-dev clang-3.5 libclang-3.5-dev 
+
 # then, it is possible to compile using llvm and clang with:
 # $ ./configure --enable-release CC=clang CXX=clang++ --with-pcre
+# using previous command performs more steps with clang, version 3.5,
+# but an error occurs in the linking phase.
 
+# WORKING `CONFIGURE` COMMAND: it uses gcc to compile
+# $ ./configure --enable-release --with-pcre --with-llvm-version=3.5
