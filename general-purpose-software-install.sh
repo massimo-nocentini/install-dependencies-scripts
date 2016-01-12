e9703039d1fdf6742b25c3854f3b5ba1cffc5e85
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-01-12 14:31:14 +0100

    Add `gparted` to general software install script.

--- general-purpose-software-install.sh
+++ general-purpose-software-install.sh
@@ -5,7 +5,8 @@
 # Moreover, Samsung printer drivers could be installed, too.
 # They are present under ~/Developer/drivers/samsung/
 
-sudo apt-get install calibre gaphor curl synaptic pidgin remmina
+sudo apt-get install calibre gaphor curl synaptic pidgin remmina \
+    gparted
 
 # in order to install `dropbox`, use the following command:
 # $ cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
