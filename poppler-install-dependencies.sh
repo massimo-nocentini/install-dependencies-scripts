652f64a44e0061524be6394b149a05df149e266a
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-02-19 09:33:52 +0100

    Add poppler script.

---
+++ poppler-install-dependencies.sh
@@ -0,0 +1,5 @@
+# the following command has been composed according to:
+# http://www.linuxfromscratch.org/blfs/view/svn/general/poppler.html
+sudo apt-get install fontconfig libcairo2-dev libjpeg-turbo8-dev libpng12-dev libpng3 libopenjpeg-dev 
+
+# moreover, after building poppler, it is necessary to compile poppler-data as well.
