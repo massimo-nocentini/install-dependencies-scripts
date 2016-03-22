bb95d69c66ae1491f749f1dd8aeaa721486fda72
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-03-22 21:02:10 +0100

    Refine Gnu Smalltalk script.

--- gnu-smalltalk-install-dependencies.sh
+++ gnu-smalltalk-install-dependencies.sh
@@ -1,3 +1,8 @@
 sudo apt-get install libzip-dev libsigsegv-dev libffi-dev libltdl-dev \
     libreadline-dev libgmp-dev libgnutls-dev libcairo2-dev libsdl2-dev \
-    texi2html automake autoconf m4 perl autotools-dev libtool
+    texi2html automake autoconf m4 perl autotools-dev libtool bison flex
+
+# in order to compile, type the following commands:
+# $ autoreconf -vi
+# $ ./configure
+# $ make
