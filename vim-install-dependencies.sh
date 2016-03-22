1b0037746b51caf8ad68a3864e02ab9dc27266fd
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-03-22 10:31:21 +0100

    Removed useless comment about `Vim` compilation.

--- vim-install-dependencies.sh
+++ vim-install-dependencies.sh
@@ -8,7 +8,6 @@ sudo apt-get install \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev
 
-# it should be interesting to add support for ruby too as an internal interpreter.
 ./configure --enable-luainterp=yes --with-lua-prefix=/usr/local \
     --with-features=huge --enable-python3interp=yes --enable-rubyinterp=yes \
     --enable-perlinterp=yes
