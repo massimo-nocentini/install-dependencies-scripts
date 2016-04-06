b5dc925e7ce992927b840b279f991ca532ad1261
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-04-06 12:51:55 +0200

    Add `unrar` software.

--- general-purpose-software-install.sh
+++ general-purpose-software-install.sh
@@ -7,7 +7,7 @@
 # They are present under ~/Developer/drivers/samsung/
 
 sudo apt-get install calibre gaphor curl synaptic pidgin remmina \
-    gparted chromium-browser evince-gtk gsfonts-x11
+    gparted chromium-browser evince-gtk gsfonts-x11 unrar
 
 # in order to install `dropbox`, use the following command:
 # $ cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
