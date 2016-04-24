2e09de9ba9528ac1d1cc37a3bb30550c3751dcfa
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-04-24 08:26:54 +0200

    Add a comment for `GST`.

--- gnu-smalltalk-install-dependencies.sh
+++ gnu-smalltalk-install-dependencies.sh
@@ -1,8 +1,12 @@
 sudo apt-get install libzip-dev libsigsegv-dev libffi-dev libltdl-dev \
     libreadline-dev libgmp-dev libgnutls-dev libcairo2-dev libsdl2-dev \
-    texi2html automake autoconf m4 perl autotools-dev libtool bison flex
+    texi2html automake autoconf m4 perl autotools-dev libtool bison flex \
+    gawk
 
 # in order to compile, type the following commands:
 # $ autoreconf -vi
 # $ ./configure
 # $ make
+
+# after the compilation, I usually leave the directory without removing it,
+# in order to consult and read library source code with good Smalltalk.
