b12fb780c71890fc2a0848db887198009a3b9a2e
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-06-07 10:11:10 +0200

    Refine gpe17 interrupt disabling and *libdvdcss installation*.

--- general-purpose-software-install.sh
+++ general-purpose-software-install.sh
@@ -7,7 +7,7 @@
 # They are present under ~/Developer/snapshots/drivers/samsung/
 
 sudo apt-get install vlc calibre curl synaptic pidgin remmina \
-    gparted chromium-browser evince-gtk gsfonts-x11 unrar #unetbootin
+    gparted chromium-browser evince-gtk gsfonts-x11 unrar unetbootin
 
 # in order to install `dropbox`, use the following command:
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
@@ -20,7 +20,12 @@ sudo update-initramfs -u
 # $ echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
 
 # in order to play restricted dvds:
-# sudo apt-get install libdvdread4
-# the following command no longer works:
+sudo apt-get install libdvd-pkg 
+# since it has to fetch and compile the following is necessary:
+sudo dpkg-reconfigure libdvd-pkg
+    
+# before it was necessary the following:
+# $ sudo apt-get install libdvdread4
+# and after that the following command (which now no longer works):
 # $ sudo /usr/share/doc/libdvdread4/install-css.sh
 
--- gpe17-interrupt-handling.sh
+++ gpe17-interrupt-handling.sh
@@ -2,4 +2,5 @@
 grep . -r /sys/firmware/acpi/interrupts/
 
 # to disable the interrupt
-echo "disable" > /sys/firmware/acpi/interrupts/gpe17
+@ sudo su
+$ echo "disable" > /sys/firmware/acpi/interrupts/gpe17 # in `su` console
