5cfb48b1faa767d1efd305e11ab131cbe89848bc
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-11-07 09:10:07 +0100

    Refine script to install `numpy` and `scipy` from pip.

--- pip3-install-eggs.sh
+++ pip3-install-eggs.sh
@@ -1,1 +1,5 @@
-sudo pip3 install ipython jupyter sphinx matplotlib sympy 
+
+# The commented command is to avoid compilation of `numpy` and `scipy`
+#sudo pip3 install ipython jupyter sphinx matplotlib sympy numpy scipy
+
+sudo pip3 install ipython jupyter sphinx matplotlib sympy
