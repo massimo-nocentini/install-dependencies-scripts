bf3fb04404df9601fd7d1cbcb243ec3a8e9145a1
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-12-15 09:06:25 +0100

    Add `aiohttp` and `pymc` to the list of Python eggs.

--- pip3-install-eggs.sh
+++ pip3-install-eggs.sh
@@ -2,4 +2,4 @@
 # The commented command is to avoid compilation of `numpy` and `scipy`
 #sudo pip3 install ipython jupyter sphinx matplotlib sympy numpy scipy
 
-sudo pip3 install ipython jupyter sphinx matplotlib sympy
+sudo pip3 install ipython jupyter sphinx matplotlib sympy aiohttp pymc
