b46f8380ac4969b2ec722cf98f6839f95b86334f
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-12-30 11:57:41 +0100

    Modify the location of `dotfiles` and `install-dependencies-scripts`, moved in a dedicated directory `personal` under `working-copies`.

--- dotfiles-copy.sh
+++ dotfiles-copy.sh
@@ -1,11 +1,11 @@
 
 # this script makes a copy of hidden configuration files in my fresh home directory:
 
-ln -s ~/Developer/working-copies/dotfiles/.vimrc            ~/.vimrc
-ln -s ~/Developer/working-copies/dotfiles/.emacs            ~/.emacs
-ln -s ~/Developer/working-copies/dotfiles/.bashrc_extension ~/.bashrc_extension
-ln -s ~/Developer/working-copies/dotfiles/.gitconfig        ~/.gitconfig
-ln -s ~/Developer/working-copies/dotfiles/.tmux.conf        ~/.tmux.conf
-ln -s ~/Developer/working-copies/dotfiles/.ghci             ~/.ghci
+ln -s ~/Developer/working-copies/personal-stuff/dotfiles/.vimrc            ~/.vimrc
+ln -s ~/Developer/working-copies/personal-stuff/dotfiles/.emacs            ~/.emacs
+#ln -s ~/Developer/working-copies/personal-stuff/dotfiles/.bashrc_extension ~/.bashrc_extension
+ln -s ~/Developer/working-copies/personal-stuff/dotfiles/.gitconfig        ~/.gitconfig
+ln -s ~/Developer/working-copies/personal-stuff/dotfiles/.tmux.conf        ~/.tmux.conf
+ln -s ~/Developer/working-copies/personal-stuff/dotfiles/.ghci             ~/.ghci
 
-echo "source ~/.bashrc_extension" >> ~/.bashrc
+#echo "source ~/.bashrc_extension" >> ~/.bashrc
