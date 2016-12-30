2932933e7a4b6c1c21b04d5911e5f54b9e155e23
Massimo Nocentini <massimo.nocentini@gmail.com>
2016-12-30 11:58:55 +0100

    Inline commands.

--- pip3-install-eggs.sh
+++ pip3-install-eggs.sh
@@ -2,5 +2,4 @@
 # The commented command is to avoid compilation of `numpy` and `scipy`
 
 # $ `brew install pkg-config` only for MacOS: required by `matplotlib`
-pip3.6 install numpy scipy ipython jupyter sphinx sympy aiohttp 
-pip3.6 install pymc matplotlib
+pip3.6 install numpy scipy ipython jupyter sphinx sympy aiohttp pymc matplotlib
