745db7ff8338e02e2ad6a551cac4c31dcf3c98c9
Massimo Nocentini <massimo.nocentini@gmail.com>
2017-01-10 10:19:28 +0100

    Add `requests` and `networkx` modules to be installed with `pip`.

--- pip3-install-eggs.sh
+++ pip3-install-eggs.sh
@@ -1,5 +1,3 @@
 
-# The commented command is to avoid compilation of `numpy` and `scipy`
 
-# $ `brew install pkg-config` only for MacOS: required by `matplotlib`
-pip3.6 install numpy scipy ipython jupyter sphinx sympy aiohttp pymc matplotlib
+pip3.6 install numpy scipy ipython jupyter sphinx sympy aiohttp pymc matplotlib requests networkx
